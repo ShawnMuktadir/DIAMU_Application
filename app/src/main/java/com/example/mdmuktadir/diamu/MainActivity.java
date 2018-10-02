21f2f4bd487e4c2cdcede438f6c810dcb26e9e62
Shawn Muktadir <shawn.muktadir03@gmail.com>
2018-10-03 01:12:17 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mdmuktadir/diamu/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.mdmuktadir.diamu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    public WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        makeObj();
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://www.diamu.com.bd/");
+
+        //code for opening the application
+        webView.setWebViewClient(new WebViewClient());
+
+    }
+
+    public void makeObj(){
+        webView = (WebView)findViewById(R.id.webView);
+    }
+
+    //code for back button
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack())
+        {
+            webView.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+
+    }
+}
